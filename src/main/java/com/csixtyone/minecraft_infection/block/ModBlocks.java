a5d6a786a19eab6ea050a98bd9b3fd8c34bfd834
literallyJoel <joel@jdvivian.co.uk>
2022-03-11 20:08:12 +0000

    Removed unused import

--- src/main/java/com/csixtyone/minecraft_infection/block/ModBlocks.java
+++ src/main/java/com/csixtyone/minecraft_infection/block/ModBlocks.java
@@ -10,7 +10,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
 import java.util.function.Supplier;
 
 public class ModBlocks {
