2ad49f8e5e0153f43e67585b0d019910426ce3c6
literallyJoel <joel@jdvivian.co.uk>
2022-03-11 20:07:10 +0000

    Implemented initial block and items code so that new items and blocks can now be added.

--- src/main/java/com/csixtyone/minecraft_infection/block/ModBlocks.java
+++ src/main/java/com/csixtyone/minecraft_infection/block/ModBlocks.java
@@ -9,7 +9,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistry;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
