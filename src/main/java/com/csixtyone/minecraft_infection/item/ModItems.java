90049fb71967c894f650235eb6bb6109e93cf611
literallyJoel <joel@jdvivian.co.uk>
2022-04-11 23:52:42 +0100

    Fixed bug that prevented blocks from being mined

--- src/main/java/com/csixtyone/minecraft_infection/item/ModItems.java
+++ src/main/java/com/csixtyone/minecraft_infection/item/ModItems.java
@@ -3,12 +3,15 @@ package com.csixtyone.minecraft_infection.item;
 import com.csixtyone.minecraft_infection.MinecraftInfection;
 import com.csixtyone.minecraft_infection.item.custom.InfectedCoalItem;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
+
+
     //Creates the register for storing the newly created items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MinecraftInfection.MOD_ID);
 
