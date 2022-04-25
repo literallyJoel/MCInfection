2a86aeb292f6d9ad2e290b1ad925b4a41a32ab60
TomBoslee <thomascarruthers02@gmail.com>
2022-04-25 14:54:00 +0100

    Changed spawn frequency to make more common

--- src/main/java/com/csixtyone/minecraft_infection/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/csixtyone/minecraft_infection/world/feature/ModPlacedFeatures.java
@@ -9,9 +9,9 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 public class ModPlacedFeatures {
     public static final Holder<PlacedFeature> INFECTED_ORE_PLACED = PlacementUtils.register("infected_ore_placed",
             ModConfiguredFeatures.INFECTED_ORE, ModOrePlacement.commonOrePlacement(4, // VeinsPerChunk
-                    HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80))));
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80))));
 
-    public static final Holder<PlacedFeature> PUREDUST_ORE_PLACED = PlacementUtils.register("puredust_placed",
+    public static final Holder<PlacedFeature> PUREDUST_ORE_PLACED = PlacementUtils.register("puredust_ore_placed",
             ModConfiguredFeatures.PUREDUST_ORE, ModOrePlacement.commonOrePlacement(7, // VeinsPerChunk
-                    HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80))));
+                    HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80))));
 }
